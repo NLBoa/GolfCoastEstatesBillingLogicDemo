0230ed9318b0897416dc2872122c54c85115eb50
Noah Leibovitz <noah.leibovitz@outlook.com>
2026-07-10 16:52:13 -0700

    first commit

---
+++ Main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GolfCoastEstatesBillingLogicDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Golf Coast Estates Billing Logic Demo");
+        }
+    }
+}
